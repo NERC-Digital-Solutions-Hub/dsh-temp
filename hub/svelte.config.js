ac690a57ede57d0e1b327ca8b2c29ed8c46bdddc
pigeonwatcher <benjamincurry777@gmail.com>
2025-12-12 09:50:19 +0000

    chore: updated svelte config to remove docs paths

--- hub/svelte.config.js
+++ hub/svelte.config.js
@@ -13,11 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		//adapter: adapter()
 		adapter: adapter({
-			pages: './docs',
-			assets: './docs',
 			fallback: '404.html',
-			precompress: false,
-			strict: true,
 			paths: {
 				base: '/dsh-temp'
 			}
